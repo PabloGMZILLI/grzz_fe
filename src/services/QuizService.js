879fe47ff987bda77b7aadb9ce91524e2da1f43d
Matheus H. Potrich <matheushp.com@hotmail.com>
2021-05-30 13:41:44 -0300

    Quiz Service

---
+++ src/services/QuizService.js
@@ -0,0 +1,92 @@
+import axios from "../instances/axios";
+
+export function getQuizzes() {
+    return new Promise((resolve, reject) => {
+        axios
+            .get("/quiz")
+            .then((response) => resolve(response.data))
+            .catch((error) => reject(error));
+    });
+}
+
+export function createFullQuiz(quiz, adminId) {
+    return new Promise((resolve, reject) => {
+        axios
+            .post("/quiz/create", quiz, { headers: { user_id: adminId } })
+            .then((response) => resolve(response.data))
+            .catch((error) => reject(error));
+    });
+}
+
+export function addQuestion(quizId, question, adminId) {
+    return new Promise((resolve, reject) => {
+        axios
+            .post(`/quiz/add_question/${quizId}`, question, {
+                headers: { user_id: adminId },
+            })
+            .then((response) => resolve(response.data))
+            .catch((error) => reject(error));
+    });
+}
+
+export function setCorrectAnswer(questionId, answerId, adminId) {
+    return new Promise((resolve, reject) => {
+        axios
+            .post(
+                "/answer/set/correct",
+                {
+                    question_id: questionId,
+                    answer_id: answerId,
+                },
+                { headers: { user_id: adminId } }
+            )
+            .then((response) => resolve(response.data))
+            .catch((error) => reject(error));
+    });
+}
+
+export function addAnswer(quizId, answerText, adminId, isCorrect = false) {
+    return new Promise((resolve, reject) => {
+        axios
+            .post(
+                `/answer/add/${quizId}`,
+                { answer: answerText, correct: isCorrect },
+                { headers: { user_id: adminId } }
+            )
+            .then((response) => resolve(response.data))
+            .catch((error) => reject(error));
+    });
+}
+
+export function deleteQuestion(questionId, adminId) {
+    return new Promise((resolve, reject) => {
+        axios
+            .delete(`/quiz/delete_question/${questionId}`, {
+                headers: { user_id: adminId },
+            })
+            .then((response) => resolve(response.data))
+            .catch((error) => reject(error));
+    });
+}
+
+export function deleteQuiz(quizId, adminId) {
+    return new Promise((resolve, reject) => {
+        axios
+            .delete(`/quiz/delete/${quizId}`, {
+                headers: { user_id: adminId },
+            })
+            .then((response) => resolve(response.data))
+            .catch((error) => reject(error));
+    });
+}
+
+export function deleteAnswer(answerId, adminId) {
+    return new Promise((resolve, reject) => {
+        axios
+            .delete(`/answer/delete/${answerId}`, {
+                headers: { user_id: adminId },
+            })
+            .then((response) => resolve(response.data))
+            .catch((error) => reject(error));
+    });
+}
